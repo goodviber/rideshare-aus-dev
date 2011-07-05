c75529feaa6a4fcf8f7986d9642c212807dec696
Adam Kirk <adamkirk85@gmail.com>
2011-07-05 16:16:34 +1000

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cocoride::Application.config.secret_token = 'd2c91fb5d4fd4d55f044545e32eb9934e4398ad912218ba5b93f820e1c5ff4acf66142199d7ce60a8652c63b5029aa5df09006fcd0348a81d4ba27dc171f4db9'
