878e0c3bf1526b389c3392064bb2fb50762189a0
Leslie T <leslie_tanengsy@yahoo.com>
2012-11-15 13:01:50 +0800

    Edit paperclip options

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,6 @@ class Photo < ActiveRecord::Base
 
   belongs_to :event
 
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "50x50>" }
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "50x50>", :url => "/system/:class/:property_id/:id/:style/:basename.:extension",
+   :path => ":rails_root/public/system/:class/:property_id/:id/:style/:basename.:extension" }
 end
